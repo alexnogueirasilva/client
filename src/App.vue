e7987a7f45e915cf5c8c57370f06b55b417a376e
Alex Nogueira <alex@devaction.com.br>
2022-12-21 23:45:26 -0300

    feat(Home): commit initial

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,77 @@
-<script>
+<script setup lang="ts">
 import axios from "axios";
-export default {
-  data() {
-    return {
-      board: {
-        name: "Project A",
-        columns: [
-          {
-            name: "Todo",
-            cards: [
-              { title: "A", estimative: 3},
-              { title: "A", estimative: 3},
-              { title: "A", estimative: 3}
-            ]
-          },
-          {
-            name: "In Progress",
-            cards: []
-          },
-          {
-            name: "Done",
-            cards: []
-          },
-        ]
-      },
-      columnName: "",
-    }
-  },
-  methods: {
-    addColumn(columnName) {
-      this.board.columns.push({
-        name: columnName,
-        cards: []
-      });
-      this.columnName = "";
-    },
+import {computed, onMounted, reactive} from "vue";
 
-  },
-  async mounted() {
-   await axios.get("http://localhost:3000/boards/1")
-      .then(response => {
-        this.board = response.data;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+let data: any = reactive({});
+
+let columnName = "";
+let cardTitle = "";
+
+function addColumn(columnName: string) {
+  data.board.columns.push({
+    name: columnName,
+    estimative: 0,
+    cards: []
+  });
 }
+
+function addCard(column: any, cardTitle: string) {
+  column.cards.push({
+    title: cardTitle,
+    estimative: 0
+  })
+}
+
+function incraseEstmative(column: any) {
+  column.estimative++;
+}
+
+function decraseEstmative(column: any) {
+  column.estimative--;
+}
+
+const boardEstimative = computed(() => {
+ return data.board.columns.reduce((acc: number, column: any) => {
+    acc += column.cards.reduce((acc: number, card: any) => {
+      acc += card.estimative;
+      return acc;
+    }, 0);
+    return acc;
+ }, 0);
+})
+
+onMounted(async () => {
+  const response = await axios({
+    method: "GET",
+    url: "http://localhost:3000/boards/1"
+  });
+ data.board = response.data;
+})
 </script>
 
 <template>
-  <h3>{{ board.name }}</h3>
+  <div v-if="data.board">
+  <h3>{{ data.board.name }} {{ boardEstimative }}</h3><br>
   <div class="columns">
-    <div class="column" v-for="column in board.columns">
-     <h3> {{ column.name}} {{ column.estimative }}</h3>
+    <div class="column" v-for="column in data.board.columns">
+      <h3> {{ column.name }} {{ column.estimative }}</h3>
       <div class="card" v-for="card in column.cards">
-          {{ card.title }} {{ card.estimative }}
+        {{ card.title }} {{ card.estimative }}
+        <br/>
+        <button @click="incraseEstmative(card)">+</button>
+        <button @click="decraseEstmative(card)">-</button>
+      </div>
+      <div class="card new-card">
+        <input type="text" v-model="cardTitle">
+        <button v-on:click="addCard(column, cardTitle)">add</button>
       </div>
     </div>
     <div class="column new-column">
       {{ columnName }}
       <input type="text" v-model="columnName">
-      <button v-on:click="addColumn(columnName)" i>add</button>
+      <button v-on:click="addColumn(columnName)">add</button>
     </div>
+  </div>
   </div>
 </template>
 
@@ -72,12 +81,14 @@ export default {
   flex-direction: row;
   justify-content: space-between;
 }
-.new-column{
+
+.new-column {
   background-color: #EEE;
   board: 1px dashed #CCC;
   display: block;
 
 }
+
 .column {
   width: 30%;
   border: 1px solid #ccc;
@@ -88,7 +99,7 @@ export default {
   margin-right: 5px;
 }
 
-.card{
+.card {
   border: 1px solid #ccc;
   border-radius: 5px;
   padding: 10px;
@@ -99,5 +110,11 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-around;
+}
+
+.new-card {
+  background-color: #EEE;
+  border: 1px dashed #CCC;
+  display: block;
 }
 </style>
